df3f26d44be9f37499f233cb4cc3651bfb8ff864
안승태 <sng_a@naver.com>
2022-11-19 18:16:28 +0900

    [add] admin 공지사항 기능 추가

--- src/components/board/BoardView.vue
+++ src/components/board/BoardView.vue
@@ -9,10 +9,10 @@
       <b-col class="text-left">
         <b-button variant="outline-primary" @click="moveList">목록</b-button>
       </b-col>
-      <!-- <b-col class="text-right" v-if="userInfo.userid === admin"> -->
+      <b-col class="text-right" v-if="userInfo.uid === `admin`">
         <b-button variant="outline-info" size="sm" @click="moveModifyArticle" class="mr-2">글수정</b-button>
         <b-button variant="outline-danger" size="sm" @click="deleteArticle">글삭제</b-button>
-      <!-- </b-col> -->
+      </b-col>
     </b-row>
     <b-row class="mb-1">
       <b-col>
@@ -54,6 +54,7 @@ export default {
     },
   },
   created() {
+    console.log(this.userInfo.uid);
     let param = this.$route.params.bid;
     getArticle(
       param,
